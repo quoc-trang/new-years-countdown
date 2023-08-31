0c1c77af94d6b53f8e1cd4e6647d2908eb0b70c4
trang <trang.lq@raksul.com>
2023-08-31 15:53:20 +0700

    trang - initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import CountdownHeader from "@/components/CountdownHeader.vue";
+import CountdownSegment from "@/components/CountdownSegment.vue";
+</script>
+<template>
+  <div class="app-wrapper">
+    <div class="countdown-box">
+      <CountdownHeader />
+      <main class="flex justify-center">
+        <CountdownSegment data-test="days" label="days" number="0" />
+        <CountdownSegment data-test="hours" label="hours" number="0" />
+        <CountdownSegment data-test="minutes" label="minutes" number="0" />
+        <CountdownSegment data-test="seconds" label="seconds" number="0" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app-wrapper {
+  @apply flex items-center justify-center w-full h-full p-10;
+}
+.countdown-box {
+  @apply shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px];
+}
+body {
+  @apply bg-gray-100;
+}
+</style>
